b1d0d76f993dc20d3044f1f40c1e258ca52cf9fa
emontes22 <ervey025@gmail.com>
2023-09-08 22:39:05 -0600

    Initial commit

---
+++ src/test/java/com/stock_market_analyzer/Stock_Market_Analyzer/StockMarketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_market_analyzer.Stock_Market_Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
